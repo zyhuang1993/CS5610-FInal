38ed2cc82c114808976703655d523ba58bc445dd
Yue Wang <wang.yue8@husky.neu.edu>
2019-04-14 17:17:33 -0700

    movie schema changed

--- server/models/movie/movie.schema.server.js
+++ server/models/movie/movie.schema.server.js
@@ -4,6 +4,7 @@ var reviewSchema = require('../review/review.schema.server');
 var movieSchema = new mongoose.Schema({
     title: String,
     rate: Number,
+    db_id: String,
     reviews: [reviewSchema],
     dateCreated: {type:Date, default: Date.now()},
   },
