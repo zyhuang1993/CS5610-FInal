e413ced99df04ac2f1805e363008f5257824ae00
Yue Wang <wang.yue8@husky.neu.edu>
2019-04-16 23:59:19 -0700

    review-list added

--- src/app/views/review/review-list/review-list.component.spec.ts
+++ src/app/views/review/review-list/review-list.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TopReivewsComponent } from './top-reivews.component';
+import { ReviewListComponent } from './review-list.component';
 
-describe('TopReivewsComponent', () => {
-  let component: TopReivewsComponent;
-  let fixture: ComponentFixture<TopReivewsComponent>;
+describe('ReviewListComponent', () => {
+  let component: ReviewListComponent;
+  let fixture: ComponentFixture<ReviewListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TopReivewsComponent ]
+      declarations: [ ReviewListComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TopReivewsComponent);
+    fixture = TestBed.createComponent(ReviewListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
