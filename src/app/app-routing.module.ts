6f4a8bbe5b0b4084e18f861cf988c15d02b138fd
Yue Wang <wang.yue8@husky.neu.edu>
2019-04-17 15:48:31 -0700

    profile css completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import {LoginComponent} from './views/user/login/login.component';
 import {UserListComponent} from './views/user/user-list/user-list.component';
 import {MovieDetailComponent} from './views/movie/movie-detail/movie-detail.component';
 import {ProfileComponent} from './views/user/profile/profile.component';
+import {FavoriteMovieComponent} from './views/movie/favorite-movie/favorite-movie.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -15,7 +16,9 @@ const routes: Routes = [
   {path: 'profile', component: ProfileComponent},
   {path: 'login', component: LoginComponent},
   {path: 'topMovies', component: MovieListComponent},
-  {path: 'movie/:movieId', component: MovieDetailComponent}
+  {path: 'movie/:movieId', component: MovieDetailComponent},
+  {path: 'user-list', component: UserListComponent},
+  {path: 'favorite-movie', component: FavoriteMovieComponent}
 ];
 
 @NgModule({
---
+++ src/app/views/movie/favorite-movie/favorite-movie.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-favorite-movie',
+  templateUrl: './favorite-movie.component.html',
+  styleUrls: ['./favorite-movie.component.css']
+})
+export class FavoriteMovieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
