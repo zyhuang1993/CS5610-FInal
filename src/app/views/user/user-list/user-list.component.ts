86bb844247b99d0f13d653d7702423a61bc4874d
Yue Wang <wang.yue8@husky.neu.edu>
2019-04-18 11:52:10 -0700

    follower and following

--- src/app/views/user/user-list/user-list.component.ts
+++ src/app/views/user/user-list/user-list.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
-
+  users: [any];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  deleteUser() {
+
   }
 
 }
