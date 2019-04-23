4f6c5e20822839659872877ad88b83f8a60aa454
Yue Wang <wang.yue8@husky.neu.edu>
2019-04-23 04:02:21 -0700

    follow bug fixed

--- server/services/user.service.server.js
+++ server/services/user.service.server.js
@@ -383,25 +383,73 @@ module.exports = function (app) {
                           }
                           return now.then((response) => {
                             res.json(user);
-                            // userModel
-                              // .deleteUser(userId)
-                              // .then(function (user) {
-                              //   res.status(200).send(user);
-                              // }, function (error) {
-                              //   console.log('delete user by Id error: ' + error);
-                              //   res.status(200).send({message: 'User not found!'});
-                              // });
                           });
                         } else {
-                          res.json(user);
+                            res.json(user);
                         }
                       })
                     } else {
-                      res.json(user);
+                      if (user.following && user.following.length > 0) {
+                        index = 0;
+                        now = userModel.deleteFollowerById(user._id, user.following[index]);
+                        for (var i = 1; i < user.following.length; i++) {
+                          now = now.then((res) => {
+                            index++;
+                            return userModel.deleteFollowerById(user._id, user.following[index]);
+                          });
+                        }
+                        return now.then((response) => {
+                          res.json(user);
+                        });
+                      } else {
+                        res.json(user);
+                      }
                     }
                   })
                 } else {
-                  res.json(user);
+                  if (user.follower && user.follower.length > 0) {
+                    index = 0;
+                    now = userModel.deleteFollowingById(user._id, user.follower[index]);
+                    for (var i = 1; i < user.follower.length; i++) {
+                      now = now.then((response) => {
+                        index++;
+                        return userModel.deleteFollowingById(user._id, user.follower[index]);
+                      });
+                    }
+                    return now.then((response) => {
+                      if (user.following && user.following.length > 0) {
+                        index = 0;
+                        now = userModel.deleteFollowerById(user._id, user.following[index]);
+                        for (var i = 1; i < user.following.length; i++) {
+                          now = now.then((res) => {
+                            index++;
+                            return userModel.deleteFollowerById(user._id, user.following[index]);
+                          });
+                        }
+                        return now.then((response) => {
+                          res.json(user);
+                        });
+                      } else {
+                        res.json(user);
+                      }
+                    })
+                  } else {
+                    if (user.following && user.following.length > 0) {
+                      index = 0;
+                      now = userModel.deleteFollowerById(user._id, user.following[index]);
+                      for (var i = 1; i < user.following.length; i++) {
+                        now = now.then((res) => {
+                          index++;
+                          return userModel.deleteFollowerById(user._id, user.following[index]);
+                        });
+                      }
+                      return now.then((response) => {
+                        res.json(user);
+                      });
+                    } else {
+                      res.json(user);
+                    }
+                  }
                 }
               }
             )
